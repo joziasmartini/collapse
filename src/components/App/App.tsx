3779d1d7271fb8f84399039c2a0bfd72209abf6d
Jozias Martini <jozias.martini@gmail.com>
2022-10-12 21:54:46 -0300

    Move all the html and css

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,64 @@
-import './App.css';
+import './App.sass';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-     asd
-    </div>
+    <main className="interface">
+        <div className="stats">
+            <div className="health">
+                <span className="info-title">Health</span>
+                <span className="info-data" x-text="player.health"></span>
+            </div>
+            <div className="sleep">
+                <span className="info-title">Sleep</span>
+                <span className="info-data" x-text="player.sleep"></span> 
+    
+            </div>
+            <div className="food">
+                <span className="info-title">Food</span>
+                <span className="info-data" x-text="player.food"></span> 
+            </div>
+            <div className="water">
+                <span className="info-title">Water</span>
+                <span className="info-data" x-text="player.water"></span> 
+            </div>
+        </div>
+    
+        <div className="place">
+            <img className="place-image" src="illustrations/img/abandoned-house-unsplash.png"/>
+            You are now at Abandoned House.
+        </div>
+
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+    
+        <div className="inventory">
+            <span className="info-title">Inventory</span> 
+            <div className="inventory-items">
+                <div className="inventory-item">
+                    <span className="item-data"></span>
+                </div>
+                <div className="inventory-item">
+                    <span className="item-data"></span>
+                </div>
+                <div className="inventory-item">
+                    <span className="item-data"></span>
+                </div>
+                <div className="inventory-item">
+                    <span className="item-data"></span>
+                </div>
+                <div className="inventory-item">
+                    <span className="item-data"></span>
+                </div>
+            </div>
+        </div>
+    
+        <div className="craft">
+            <i className="ph-magnifying-glass button"></i>
+            <i className="ph-wrench button"></i>
+            <i className="ph-bed button"></i>
+            <i className="ph-backpack button"></i>
+        </div>
+    </main>
   );
 }
 
