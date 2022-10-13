523dea22337be180af11d64c8ddc37901745902a
Jozias Martini <jozias.martini@gmail.com>
2022-10-12 22:09:15 -0300

    Expand interface and create navbar

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,31 +1,47 @@
 import './App.sass';
+import AbandonedConstruction from "../../assets/illustrations/dall-e/abandoned-construction.png";
 import React from "react";
 
 function App() {
+  const [image, setImage] = React.useState(AbandonedConstruction);
+  const [sleep, setHappiness] = React.useState(22);
+  const [health, setHealth] = React.useState(33);
+  const [hunger, setHunger] = React.useState(77);
+  const [thirst, setThirst] = React.useState(55);
+
   return (
     <main className="interface">
-        <div className="stats">
-            <div className="health">
-                <span className="info-title">Health</span>
-                <span className="info-data" x-text="player.health"></span>
+        <nav className="nav">
+          <section className="nav-content">
+            <div className="player">
+              <div className="player-avatar"></div>
+              <div className="player-name">Player Name</div>
+              <div className="player-exp">4870 EXP</div>
+              <div className="player-condition">(bruised)</div>
             </div>
-            <div className="sleep">
-                <span className="info-title">Sleep</span>
-                <span className="info-data" x-text="player.sleep"></span> 
-    
-            </div>
-            <div className="food">
-                <span className="info-title">Food</span>
-                <span className="info-data" x-text="player.food"></span> 
-            </div>
-            <div className="water">
-                <span className="info-title">Water</span>
-                <span className="info-data" x-text="player.water"></span> 
-            </div>
-        </div>
+            <section className="stats">
+              <div className="health">
+                  <span className="info-title">Health</span>
+                  <span className="info-data">{health}</span>
+              </div>
+              <div className="sleep">
+                  <span className="info-title">Sleep</span>
+                  <span className="info-data">{sleep}</span> 
+              </div>
+              <div className="food">
+                  <span className="info-title">Food</span>
+                  <span className="info-data">{hunger}</span> 
+              </div>
+              <div className="water">
+                  <span className="info-title">Water</span>
+                  <span className="info-data">{thirst}</span> 
+              </div>
+            </section>
+          </section>
+        </nav>
     
         <div className="place">
-            <img className="place-image" src="illustrations/img/abandoned-house-unsplash.png"/>
+            <img className="place-image" src={AbandonedConstruction}/>
             You are now at Abandoned House.
         </div>
 
