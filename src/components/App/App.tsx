7377cfb0e321b7b3bbd0e000aa98037de310bb4e
Jozias Martini <jozias.martini@gmail.com>
2022-11-07 23:19:06 -0300

    Remove content and use new component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,84 +2,16 @@ import './App.sass';
 import AbandonedConstruction from "../../assets/illustrations/dall-e/abandoned-construction-cropped.png";
 import CollapseLogo from "../../assets/collapse-bad-grunge-font.png";
 import React from "react";
+import Player from '../Player/Player';
 
 function App() {
   const [image, setImage] = React.useState(AbandonedConstruction);
 
-  const [experience, setExperience] = React.useState(randomNumber(3000, 7000));
-  const [sleep, setHappiness] = React.useState(randomNumber(0, 100));
-  const [health, setHealth] = React.useState(randomNumber(0, 100));
-  const [hunger, setHunger] = React.useState(randomNumber(0, 100));
-  const [thirst, setThirst] = React.useState(randomNumber(0, 100));
-  const [mood, setMood] = React.useState(randomNumber(0, 100));
-
-  const [light, setLight] = React.useState("HIGH");
-  const [heat, setHeat] = React.useState("LOW");
-  const [rain, setRain] = React.useState("AVERAGE");
-  const [wind, setWind] = React.useState("LOW");
-
-  function randomNumber(min: number, max: number): number {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
   return (
     <main className="interface">
         {/* <CollapseLogo /> */}
-        <nav className="nav">
-          <section className="nav-content">
-            <div className="player">
-              <div className="player-avatar"></div>
-              <div className="player-name">Player</div>
-              <div className="player-exp">EXPERIENCE {experience}</div>
-              <br/>
-            </div>
-
-            <section className="stats">
-              <div className="health">
-                  <span className="info-title">Health</span>
-                  <span className="info-data">{health}</span>
-              </div>
-              <div className="sleep">
-                  <span className="info-title">Sleep</span>
-                  <span className="info-data">{sleep}</span> 
-              </div>
-              <div className="food">
-                  <span className="info-title">Food</span>
-                  <span className="info-data">{hunger}</span> 
-              </div>
-              <div className="water">
-                  <span className="info-title">Water</span>
-                  <span className="info-data">{thirst}</span> 
-              </div>
-              <div className="water">
-                  <span className="info-title">Mood</span>
-                  <span className="info-data">{mood}</span> 
-              </div>
-            </section>
-
-            <br/>
-            <section className="environment">
-              <div className="health">
-                  <span className="info-title">Light</span>
-                  <span className="info-data">{light}</span>
-              </div>
-              <div className="health">
-                  <span className="info-title">Heat</span>
-                  <span className="info-data">{heat}</span>
-              </div>
-              <div className="health">
-                  <span className="info-title">Rain</span>
-                  <span className="info-data">{rain}</span>
-              </div>
-              <div className="health">
-                  <span className="info-title">Wind</span>
-                  <span className="info-data">{wind}</span>
-              </div>
-            </section>
-          </section>
-        </nav>
+        
+        <Player />
     
         <section className="right-side">
             <div className="place">
