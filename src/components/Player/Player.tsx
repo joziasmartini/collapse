c3b4b36697ef20f189ed206f41d0eeca01bd8255
Jozias Martini <jozias.martini@gmail.com>
2022-11-18 22:00:03 -0300

    Update utils import

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { randomNumber } from "../../assets/utils/utils";
+import { randomNumber } from "../../utils/utils";
 import "./Player.sass";
 
 export default function Player() {
