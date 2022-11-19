b19b65227efae780cc2f1ad04e559f915c1e22b1
Jozias Martini <jozias.martini@gmail.com>
2022-11-18 22:00:33 -0300

    Start creating search and explore functions

--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -1,10 +1,16 @@
 import "./Location.sass";
 // import AbandonedConstruction from "../../assets/illustrations/dall-e/abandoned-construction-cropped.png";
 import Button from "../Button/Button";
+import { useState } from "react";
 
 type Props = {};
 
 export default function Location({}: Props) {
+  const [items, setItems] = useState([]);
+
+  const searchLocation = () => {};
+  const continueExploring = () => {};
+
   return (
     <section className="right-side">
       <div className="place">
@@ -12,8 +18,8 @@ export default function Location({}: Props) {
       </div>
 
       <div className="actions">
-        <Button text="Search Location" onClick={() => {}} />
-        <Button text="Continue Exploring" onClick={() => {}} />
+        <Button text="Search Location" onClick={() => searchLocation} />
+        <Button text="Continue Exploring" onClick={() => continueExploring} />
       </div>
 
       {/* <div className="searched">
