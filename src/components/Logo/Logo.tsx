c4c409204b886cbdec0b3e884dd366b0664f2117
Jozias Martini <jozias.martini@gmail.com>
2023-05-11 20:01:11 -0300

    feat: create logo as a component

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,6 @@
+import CollapseLogo from "../../assets/collapse-bad-grunge-font.png";
+import "./Logo.sass";
+
+export default function Logo() {
+  return <img className="logo" src={CollapseLogo} alt="" />;
+}
