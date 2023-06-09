9775f9311c087b5d009a78ad8999ef37d327da80
Jozias Martini <jozias.martini@gmail.com>
2023-06-09 19:00:49 -0300

    feat: create enemies.ts

---
+++ new-app/src/enemies/enemies.ts
@@ -0,0 +1,74 @@
+export const enemies = [
+  {
+    name: "Wolf",
+    description:
+      "A fierce predator known for its pack mentality and hunting prowess.",
+    attack: 20,
+    loot: ["Wolf Pelt", "Wolf Fang"],
+    rarity: 1,
+  },
+  {
+    name: "Bear",
+    description:
+      "A powerful and territorial creature capable of inflicting significant damage.",
+    attack: 30,
+    loot: ["Bear Claw", "Bear Hide"],
+    rarity: 2,
+  },
+  {
+    name: "Crocodile",
+    description:
+      "A stealthy reptile lurking in bodies of water, ready to ambush its prey.",
+    attack: 25,
+    loot: ["Crocodile Skin", "Crocodile Tooth"],
+    rarity: 2,
+  },
+  {
+    name: "Mountain Goat",
+    description:
+      "An agile climber with sharp horns, capable of defending itself on rocky terrain.",
+    attack: 15,
+    loot: ["Goat Horn", "Goat Milk"],
+    rarity: 1,
+  },
+  {
+    name: "Pirates",
+    description:
+      "Ruthless bandits of the seas, seeking treasure and willing to fight for it.",
+    attack: 35,
+    loot: ["Pirate Hat", "Cutlass"],
+    rarity: 3,
+  },
+  {
+    name: "Mummies",
+    description:
+      "Undead creatures preserved in wrappings, cursed to guard ancient tombs.",
+    attack: 30,
+    loot: ["Mummy Bandages", "Ancient Amulet"],
+    rarity: 3,
+  },
+  {
+    name: "Bandits",
+    description:
+      "Outlaws and thieves who roam the city, preying on the weak and unwary.",
+    attack: 25,
+    loot: ["Bandit Mask", "Stolen Coins"],
+    rarity: 2,
+  },
+  {
+    name: "Alligator",
+    description:
+      "A large reptile lurking in swamps and rivers, with a powerful bite.",
+    attack: 30,
+    loot: ["Alligator Skin", "Alligator Tooth"],
+    rarity: 2,
+  },
+  {
+    name: "Wild Boar",
+    description:
+      "A ferocious and territorial beast known for its charging attacks.",
+    attack: 20,
+    loot: ["Boar Tusk", "Boar Meat"],
+    rarity: 1,
+  },
+];
