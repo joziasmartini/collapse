f5d49114f3b04df30d62b504cb34597e2d4a2cd9
Jozias Martini <jozias.martini@gmail.com>
2023-06-09 19:00:56 -0300

    feat: create places.ts

---
+++ new-app/src/places/places.ts
@@ -0,0 +1,70 @@
+export const places = [
+  {
+    name: "Abandoned House",
+    description: "An old house that has been left vacant for a long time.",
+    loot: ["Flashlight", "Canned Food", "Medkit"],
+    enemies: ["Zombie"],
+  },
+  {
+    name: "Forest",
+    description: "A dense and expansive forest with various flora and fauna.",
+    loot: ["Knife", "Berries", "Animal Hide"],
+    enemies: ["Wolf", "Bear"],
+  },
+  {
+    name: "Cave",
+    description:
+      "A dark and mysterious cave with hidden treasures and dangers.",
+    loot: ["Gemstone", "Rope", "Torch"],
+    enemies: ["Giant Spider", "Bats"],
+  },
+  {
+    name: "Lake",
+    description:
+      "A serene lake surrounded by nature, offering a source of water and potential fishing spots.",
+    loot: ["Fishing Rod", "Water Bottle", "Cooking Pot"],
+    enemies: ["Crocodile", "Snakes"],
+  },
+  {
+    name: "Mountains",
+    description:
+      "A towering mountain range with challenging terrain and breathtaking views.",
+    loot: ["Climbing Gear", "Herbs", "Binoculars"],
+    enemies: ["Mountain Goat", "Eagle"],
+  },
+  {
+    name: "Deserted Island",
+    description:
+      "A remote island with sandy beaches, lush vegetation, and limited resources.",
+    loot: ["Coconut", "Spear", "Shelter Blueprint"],
+    enemies: ["Pirates", "Sharks"],
+  },
+  {
+    name: "Ruins",
+    description:
+      "Ancient ruins filled with remnants of past civilizations and potential hidden artifacts.",
+    loot: ["Ancient Relic", "Map Fragment", "Treasure Chest"],
+    enemies: ["Mummies", "Ghost"],
+  },
+  {
+    name: "City",
+    description:
+      "A sprawling urban environment with both abandoned and occupied buildings.",
+    loot: ["Backpack", "Crowbar", "Gas Mask"],
+    enemies: ["Bandits", "Mutants"],
+  },
+  {
+    name: "Creek",
+    description:
+      "A small flowing creek with fresh water and potential wildlife nearby.",
+    loot: ["Water Filter", "Fishing Net", "Fire Starter"],
+    enemies: ["Alligator", "Poisonous Frogs"],
+  },
+  {
+    name: "Countryside Farm",
+    description:
+      "A peaceful farm in the countryside with fertile land and farm animals.",
+    loot: ["Vegetables", "Farm Tools", "Livestock"],
+    enemies: ["Wild Boar", "Coyote"],
+  },
+];
