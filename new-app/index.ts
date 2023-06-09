f9bbdea050b44c9fbfdba8ec94b361cff08a97c1
Jozias Martini <jozias.martini@gmail.com>
2023-06-09 19:00:44 -0300

    feat: create enemies.ts

---
+++ new-app/index.ts
@@ -0,0 +1,6 @@
+import { player } from "./src/player/player";
+import { foods } from "./src/items/foods";
+import { drinks } from "./src/items/drinks";
+import { events } from "./src/events/events";
+import { places } from "./src/places/places";
+import { enemies } from "./src/enemies/enemies";
